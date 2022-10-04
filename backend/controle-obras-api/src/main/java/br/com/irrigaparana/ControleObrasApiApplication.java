5d7be6b8ef19b194586e37a7b3e83756b17d14a9
Lucas Guilha <medowlucas@gmail.com>
2022-10-03 22:25:14 -0300

    criando o projeto springboot

---
+++ backend/controle-obras-api/src/main/java/br/com/irrigaparana/ControleObrasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.irrigaparana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleObrasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleObrasApiApplication.class, args);
+	}
+
+}
---
+++ backend/controle-obras-api/src/test/java/br/com/irrigaparana/ControleObrasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.irrigaparana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleObrasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
